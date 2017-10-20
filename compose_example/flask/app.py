0d22d953070c5021806439301b995b3d33788dd7
Anthony <anthony.r.buonomo@nasa.gov>
2017-10-20 12:27:24 -0400

    app comment

--- compose_example/flask/app.py
+++ compose_example/flask/app.py
@@ -10,4 +10,5 @@ app = Flask(__name__)
 def hello_world():
         return '“Space,” it says, “is big. Really big. You just won’t believe how vastly, hugely, mindbogglingly big it is. I mean, you may think it’s a long way down the road to the chemist’s, but that’s just peanuts to space.”'
 
+# Must run on 0.0.0.0 to be visible to other containers.
 app.run(host="0.0.0.0", port=PORT)
